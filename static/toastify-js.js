479846deb962c28dceb5286f7e8cf1e46ed3640e
INSPIRED ENGINEER <kaigwaakram123@gmail.com>
2026-09-01 23:43:37 +0300

    Correct Pro bootstrap script loading

--- static/toastify-js.js
+++ static/toastify-js.js
@@ -1,7 +1,7 @@
 /* Joes Studio bootstrap: preserve Toastify and load Pro activation modules before the main app initializes. */
 (function () {
   'use strict';
-  document.write('<script src="./static/toastify-core.js"><\\/script>');
-  document.write('<script src="./static/joes-studio-pro.js"><\\/script>');
-  document.write('<script src="./static/joes-studio-premium.js"><\\/script>');
+  document.write('<script src="./static/toastify-core.js"><\/script>');
+  document.write('<script src="./static/joes-studio-pro.js"><\/script>');
+  document.write('<script src="./static/joes-studio-premium.js"><\/script>');
 })();
